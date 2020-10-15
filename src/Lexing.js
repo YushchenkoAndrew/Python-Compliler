6d73cf09e213e396835bf2a8929d8b12896e81c8
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-10-15 22:30:16 +0300

    Create a new solution for definding tokens

--- src/Lexing.js
+++ src/Lexing.js
@@ -1,5 +1,5 @@
 const { readFileSync } = require("fs");
-const lexemes = require("./TokenTypes.json");
+var lexemes = require("./TokenTypes.json");
 
 class Lexing {
   constructor(path) {
@@ -7,121 +7,139 @@ class Lexing {
 
     // Text filtration
     // Read code and delete all commentaries from it
-    this.lines = readFileSync(path, "utf-8")
-      .split("\n")
-      .map((line) => {
+    this.lines = textFiltration(readFileSync(path, "utf-8"));
+    this.tokens = [];
+
+    // Additional Functions
+    function textFiltration(text) {
+      return text.split("\n").map((line) => {
         line = `${line} `.slice(0, line.indexOf("#"));
 
         // Delete Commentaries on top of that if line become empty
         // after deleting commentary that it will 'erase' it
         // in other word replace with empty string
-        if (line.replace(/ /g, "").length) return line;
-        return "";
+        return line.replace(/ /g, "").length ? line : "";
       });
-    this.text = this.lines.join("");
-    console.log(this.text);
-    // console.log(lexemes);
-    this.lineNum = 0;
-    this.delta = 0;
-
-    this.tokens = [];
+    }
   }
 
-  isNewLine(i) {}
-
   defineTokens() {
-    let str = "";
-    let variable = "";
-    let i = 0;
+    for (let i in this.lines) {
+      let j = 0;
+      let variable = "";
+      let currLine = [];
 
-    while (i < this.text.length) {
-      str += this.text[i];
+      while (this.lines[i][j] !== undefined) {
+        // Find lexemes in the code
+        let result = findLexeme(i, j, this.lines[i]);
 
-      var prevLine = this.lineNum;
+        switch (result.length) {
+          // If there sever possible chooses then find the biggest one and save it
+          case 2:
+            result = [result[this.maxValueIndex(result)]];
 
-      // Need a while loop for come over '\n' symbol, because in the this.text it's missed
-      // That mean that al symbols '\n' erased, while loop will find the spot where this.text[i]
-      // located in other words it' find the specific lineNum despite empty one
-      while (this.lines[this.lineNum][i - this.delta] != this.text[i]) {
-        this.delta += this.lines[this.lineNum].length;
-        this.lineNum++;
-      }
+          case 1:
+            // Add a variable, which is not defined in TokenTypes
+            if (variable) currLine.push(defineValue({ value: variable, type: "Variable", line: Number(i), char: j - variable.length }, currLine));
 
-      // Python can handle Not ASCII Character!!
-      // Handler of not ASCII Symbols
-      // if (this.text[i].charCodeAt(0) > 127) {
-      //   console.log("Nooooooooo");
-      //   break;
-      // }
+            currLine.push(defineSyntaxToken(result[0], currLine));
+            j += result[0].value.length;
 
-      // Find tokens in the code
-      let result = [];
-      for (let j in lexemes) {
-        let begin = i - str.length + 1;
-
-        if (j.includes(str) && j == this.text.substring(begin, begin + j.length))
-          result.push({ value: j, type: lexemes[j], line: this.lineNum, char: this.lines[this.lineNum].indexOf(j) });
-      }
-
-      // if (result.length) console.log(result);
-
-      switch (result.length) {
-        // Some symbol not exist
-        case 0: {
-          if (variable.length && prevLine != this.lineNum) {
-            this.tokens.push({ value: variable, type: "Variable", line: prevLine, char: this.lines[prevLine].indexOf(variable) });
             variable = "";
-          }
+            break;
 
-          let { type } = this.tokens[this.tokens.length - 1] || { type: "" };
-          if (type == "Define Char") this.tokens[this.tokens.length - 1].value += this.text[i];
-          else variable += str;
-
-          str = "";
-          i++;
-          break;
+          default:
+            variable += this.lines[i][j];
+            j++;
         }
-        case 1: {
-          // Add a variable, variable is not define in TokenTypes
-          // if (variable.length && variable.replace(/\ /g, "").length) {
-          if (variable.length) {
-            this.tokens.push({ value: variable, type: "Variable", line: prevLine, char: this.lines[prevLine].indexOf(variable) });
-            variable = "";
-          }
-
-          let { type, line, char } = this.tokens[this.tokens.length - 1] || { type: "", line: 0, char: 0 };
-
-          // Check if curr and prev items is a Number if so then add them together
-          if (result[0].type.includes("Number") && type.includes("Number")) {
-            let prev = this.tokens.pop();
-            let { value } = result[0];
-            this.tokens.push({ value: prev.value + value, type: value == "." ? result[0].type : prev.type, line: prev.line, char: prev.char });
-          } else {
-            // Check if prev item was a Char
-            if (type == "Define Char") {
-              let { value } = this.tokens.pop();
-              // Check if String is ended, if so then make a proper cut and define correct type
-              //    else statement need for adding str which is define in TokenTypes.json
-              if ((result[0].value == "'" || result[0].value == '"') && result[0].value == value[0])
-                this.tokens.push({ value: value.substring(1), type: value.length > 2 ? "String" : "Char", line: line, char: char });
-              else this.tokens.push({ value: value + result[0].value, type: type, line: line, char: char });
-            } else this.tokens.push(result[0]);
-          }
-
-          i += result[0].value.length - str.length + 1;
-
-          str = "";
-          break;
-        }
-        // If some symbol meets twice or more, then try get more information
-        default:
-          i++;
       }
+
+      if (variable) currLine.push(defineValue({ value: variable, type: "Variable", line: Number(i), char: j - variable.length }, currLine));
+
+      // console.log(currLine);
+      if (currLine[0]) this.tokens.push(currLine);
     }
 
     console.log(this.tokens);
-
     return this.tokens;
+
+    //
+    // Additional functions
+    //
+    function findLexeme(i, j, line) {
+      let result = [];
+      for (let k in lexemes) {
+        if (k.includes(line[j] || " ") && k == line.substring(j, j + k.length)) result.push({ value: k, type: lexemes[k], line: Number(i), char: j });
+      }
+      return result;
+    }
+
+    function defineValue(token, currLine) {
+      let prev = currLine.slice(-1)[0] || { type: "" };
+      if (prev.type.includes("Number")) {
+        // Update prev token
+        currLine.splice(-1);
+        prev.value += token.value;
+        return defineVariable(prev, currLine);
+      } else if (!isNaN(token.value)) token.type = token.value.includes(".") ? "Float Number" : "Number";
+
+      return defineVariable(token, currLine);
+    }
+
+    function defineSyntaxToken(token, currLine) {
+      if (token.type == "Quote Mark") {
+        let index = -1;
+        for (let i = 0; i < currLine.length; i++)
+          if (token.value == currLine[i].value && currLine[i].type == "Quote Mark") {
+            index = i;
+            break;
+          }
+
+        if (index != -1) {
+          let arr = currLine.splice(index + 1);
+
+          // Get a char number of the string
+          let { line, char } = currLine.pop();
+          let str = "";
+          for (let i in arr) str += arr[i].value;
+
+          return { value: str, type: "String", line: line, char: char };
+        }
+      }
+
+      if (includeArray(token.type, "Number", "Variable", "Keyword")) return defineVariable(token, currLine);
+      return token;
+    }
+
+    function defineVariable(token, currLine) {
+      let prev = currLine.slice(-1)[0];
+      if (prev && includeArray(prev.type, "Variable", "Keyword")) {
+        // Update prev token
+        currLine.splice(-1);
+        prev.value += token.value;
+        prev.type = "Variable";
+        return prev;
+      }
+      return token;
+    }
+
+    function includeArray(value, ...arr) {
+      for (let el of arr) if (value.includes(el)) return true;
+      return false;
+    }
+  }
+
+  maxValueIndex(arr) {
+    let index = 0;
+    let { value } = arr[index];
+
+    for (let i = 1; i < arr.length; i++)
+      if (value.length < arr[i].value.length) {
+        index = i;
+        value = arr[i].value.length;
+      }
+
+    return index;
   }
 
   getTokens() {
@@ -130,7 +148,6 @@ class Lexing {
 
   showTable() {
     console.log(`\n   Value\t|\tType\n${"_".repeat(16)}|${"_".repeat(35)}`);
-
     for (let i in this.tokens) console.log(`   '${this.tokens[i].value}'  \t|\t${this.tokens[i].type}`);
   }
 }
